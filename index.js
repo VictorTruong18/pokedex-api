f2db687607ee7a28bcf79cfab228cf0dc647393f
VictorTruong18 <victor.truong18@gmail.com>
2022-12-27 12:56:08 +0100

    Finish Setup Chapter

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import config from './config.js';
+
+// Models import 
 import Dresseur from './models/dresseur.js';
 import Pokemon from './models/pokemon.js';
 
-// Model relationship
+// Routes import
+import RegisterRouter from './routes/register.js';
+
+// Model relationships
 Dresseur.hasMany(Pokemon)
 Pokemon.belongsTo(Dresseur)
 
@@ -11,9 +16,15 @@ const app = express()
 
 app.use(express.json())
 
+
+// Register route
+app.use('/register', RegisterRouter)
+
+
 // Default route
 app.use((req, res, next) => {
     res.status(404).send({"error" :"URL not found"})
 })
+
 
 app.listen(config.NODE_APP_PORT);
---
+++ routes/register.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { Router } from 'express';
+import Dresseur from './../models/dresseur.js';
+import bcrypt from 'bcrypt';
+
+const RegisterRouter = Router()
+
+RegisterRouter.post('/', async(req,res) => {
+
+    const { firstName, lastName, login, password, age, roles} = req.body 
+    try {
+        // encrypt the password
+        const encryptedPassword = bcrypt.hashSync(password, 5)
+        // create the user and return his id
+        const { id } = await Dresseur.create({
+            firstName, 
+            lastName,
+            login,
+            password: encryptedPassword,
+            age,
+        })
+        return res.status(201).send({id})
+    } catch(err){
+        console.log(err)
+        return res.status(500).send({"error" : err})
+    }
+})
+
+
+export default RegisterRouter
