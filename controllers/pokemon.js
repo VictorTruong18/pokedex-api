7e605f3d8553f083241e91c9af26ef8d9073fd05
VictorTruong18 <victor.truong18@gmail.com>
2022-12-30 19:25:24 +0100

    Add Delete Pokemon

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -17,7 +17,6 @@ export const createPokemon = async(req,res) => {
 export const modifyPokemon = async(req,res) => {
     const pokemonId = req.params.id
     const patchedInformations = req.body
-    console.log("HELLO")
     try {
         const pokemon = await Pokemon.findByPk(pokemonId)
         if(!pokemon){
@@ -28,6 +27,20 @@ export const modifyPokemon = async(req,res) => {
         return res.status(200).send(updatedPokemon)
     }catch(err){
         return res.status(500).send(err)
+    }   
+}
+
+export const deletePokemon = async(req,res)=> {
+    const id = req.params.id 
+
+    try {
+        const pokemon = await Pokemon.findById(id)
+        if(!pokemon){
+            return res.status(404).send('Pokemon not found verify your access token')
+        }
+        pokemon.destroy()
+        return res.status(200).send({'Pokemon id erased' : id })
+    } catch(error){
+        return res.status(500).send(error)
     }
-    
 }
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { Router } from 'express';
-import { createPokemon, modifyPokemon } from '../controllers/pokemon.js';
+import { createPokemon, modifyPokemon, deletePokemon } from '../controllers/pokemon.js';
 import { checkAuthorization, isDresseurAdmin, isDresseurPokemon, isAllowedToModifyOwner } from '../controllers/authorization.js';
 
 const PokemonRouter = Router()
 
 PokemonRouter.post('/', [checkAuthorization, createPokemon])
 PokemonRouter.patch('/:id', [checkAuthorization, isDresseurAdmin, isDresseurPokemon, isAllowedToModifyOwner, modifyPokemon] )
+PokemonRouter.delete('/:id', [checkAuthorization, isDresseurAdmin, isDresseurPokemon, deletePokemon] )
 
 export default PokemonRouter
